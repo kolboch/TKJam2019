e086a6afab117211fca6cf1c234407e79d089366
Maksym Tsymbal <maksym.tsymbal@volvo.com>
2019-02-23 20:52:48 +0100

    Added PostProcessing, Logo, Models

---
+++ Assets/Scripts/DistanceLogger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DistanceLogger : MonoBehaviour {
+    public Text distance;
+    private Vector3 lastPosition;
+    Vector3 startPos;
+    private float distanceTraveled;
+    // Use this for initialization
+   
+   void Start () {
+        startPos = transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        distanceTraveled = Vector3.Distance(Camera.main.transform.position, startPos);
+        //lastPosition = transform.position;
+        distance.text = distanceTraveled.ToString();
+    }
+}
