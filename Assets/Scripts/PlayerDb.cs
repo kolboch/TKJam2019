7c1e56d71fad8e06e0f1ad689104a0e65ce6a6d4
karlo <kolboch@gmail.com>
2019-02-23 20:54:22 +0100

    Implement registering and getting updates of dmg

---
+++ Assets/Scripts/PlayerDb.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDb
+{
+    public int dmgMade;
+
+    public PlayerDb()
+    {
+        dmgMade = 0;
+    }
+}
